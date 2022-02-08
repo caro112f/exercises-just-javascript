cfd850d069094bc15fb21647fba4c10d9b88782a
Caroline Nielsen <caro112f@stud.kea.dk>
2022-02-08 14:33:48 +0100

    first string exercise

---
+++ basic.js
@@ -0,0 +1,47 @@
+"use strict";
+
+console.log("test");
+
+const bool = true;
+console.log(bool);
+const num = 41;
+console.log(num);
+const str = "Peter";
+console.log(str);
+const obj = {
+  cats: 2,
+  cars: 1,
+};
+console.log(obj);
+const nothing = null;
+console.log(nothing);
+let undf;
+console.log(undf);
+const symbol = Symbol("symbol");
+console.log(symbol);
+
+const n1 = 1;
+const n2 = 2;
+
+const s1 = "1";
+const s2 = "2";
+
+n1 + n2;
+s1 + s2;
+
+n1 + s2;
+s1 + n2;
+
+"" + 1 + 0;
+"" - 1 + 0;
+true + false;
+6 / "3";
+"2" + "3";
+
+const value = " ";
+if (value) {
+  console.log("Value is truthy");
+} else {
+  console.log("Value is falsy");
+  f;
+}
---
+++ strings.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const navn = "Caroline";
+const dyrnavn = "Molly";
+
+const text = `My name is ${navn} 
+and I have a dog named ${dyrnavn}`;
+
+console.log(text);
